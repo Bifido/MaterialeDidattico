b99c4ecdb1b724d3c0c0ea32fc197c1ed5aeb631
valerik87 <valerio.ceraudo@gmail.com>
2015-04-25 10:05:13 +0200

    lezione 23-24

---
+++ Mobile Programming/Projects/ShooterTest/Assets/Script/Miei/Utils/QualityIndex.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualityIndex{
+	public Resolution 	m_Resolution;
+	public float 		m_MemSize;
+
+	QualityIndex(){
+		this.m_Resolution = Screen.currentResolution;
+		this.m_MemSize = SystemInfo.systemMemorySize;
+		//todo etc etc
+	}
+}
