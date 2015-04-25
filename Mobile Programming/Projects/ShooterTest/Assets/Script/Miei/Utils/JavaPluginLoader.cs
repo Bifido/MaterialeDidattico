5a154e32b5740663365cd80c43fb48e838b81e01
valerik87 <valerio.ceraudo@gmail.com>
2015-04-25 16:38:26 +0200

    plugin first attempt

---
+++ Mobile Programming/Projects/ShooterTest/Assets/Script/Miei/Utils/JavaPluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System;
+
+
+
+public class JavaPluginLoader : MonoBehaviour {
+	void Start(){
+		AndroidJavaClass jc = new AndroidJavaClass("ReturnClassPlugin.MainActivity");
+		jc.CallStatic("ReturnString");
+	}
+}
